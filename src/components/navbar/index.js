d2a1d59af663b8613df6f347323bcda11e3a42ae
crispimluiz <crispimluiz@live.com>
2021-03-01 13:25:51 -0300

    navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Navbar() {
   return (
@@ -12,9 +13,22 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/usuarionovo">Cadastrar</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/">Login</Link></li>
+            {//Se o usuário está logado
+              useSelector(state => state.usuarioLogado) > 0 ?
+                <>
+                  <li className="nav-item"><Link className="nav-link" to="/usuarionovo">Publicar Eventos</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/">Meus Eventos</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/">Sair</Link></li>
+                </>
+
+                : //senão
+                <>
+                  <li className="nav-item"><Link className="nav-link" to="/usuarionovo">Cadastrar</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/">Login</Link></li>
+                </>
+            }
           </ul>
         </div>
       </div>
